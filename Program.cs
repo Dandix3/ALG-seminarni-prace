50f7bbc3be6405eecac6b149dc4d3d25e949805d
Daniel Kopecký <kopeda@delta-studenti.cz>
2022-02-20 19:55:07 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,27 +20,7 @@ namespace Seminárka_ALG
 
             Console.WriteLine();
 
-            Console.WriteLine("-------------Lineární vyhledávání-------------");
-            Console.WriteLine("hledám index čísla 7");
-            Console.WriteLine(Linear(tab, 7));
-            Console.WriteLine("hledám index čísla -7");
-            Console.WriteLine(Linear(tab, -7));
-
-            Console.WriteLine("-------------Binární vyhledávání s rekurzí-------------");
-            Console.WriteLine("hledám index čísla 21");
-            Console.WriteLine(Binary_rek(tab1, 21, 0, 13));
-            Console.WriteLine("hledám index čísla 40");
-            Console.WriteLine(Binary_rek(tab1, 40, 0, 13));
-
-
-            Console.WriteLine("-------------Binární vyhledávání bez rekurze-------------");
-            Console.WriteLine("hledám index čísla 25");
-            Console.WriteLine(Binary(tab1, 25));
-            Console.WriteLine("hledám index čísla 29");
-            Console.WriteLine(Binary(tab1, 29));
-
-            Console.WriteLine("---------------------------------------------------------");
-
+            #region FIFO
             Console.WriteLine("---------------------FIFO fronta------------------");
             Queue<int> fifo = new Queue<int>();
             Console.WriteLine("Vlozím do fronty 1 poté 2 a 3, a první číslo které se mi vrátí je 1");
@@ -49,16 +29,20 @@ namespace Seminárka_ALG
             fifo.Enqueue(3);
 
             Console.WriteLine(fifo.Dequeue());
+            #endregion
 
+            #region LIFO
             Console.WriteLine("---------------------LIFO fronta------------------");
             Stack<int> lifo = new Stack<int>();
             Console.WriteLine("Vlozím do zásobníku 1 poté 2 a 3, a první číslo které se mi vrátí je 3");
             lifo.Push(1);
             lifo.Push(2);
             lifo.Push(3);
-            
+
             Console.WriteLine(lifo.Pop());
-            
+            #endregion
+
+            #region Spojový seznam
             Console.WriteLine("-------------------Spojový seznam-----------------");
             LinkedList<int> seznam = new LinkedList<int>();
             seznam.AddFirst(1);
@@ -69,15 +53,41 @@ namespace Seminárka_ALG
             seznam.AddAfter(prostredni, 32);
             seznam.AddAfter(prostredni, 31);
             seznam.Remove(prostredni);
-            
             foreach (int i in seznam)
             {
                 Console.Write($"{i}, ");
             }
+            Console.WriteLine();
+            #endregion
+
+            #region Linear
+            Console.WriteLine("-------------Lineární vyhledávání-------------");
+            Console.WriteLine("hledám index čísla 7");
+            Console.WriteLine(Linear(tab, 7));
+            Console.WriteLine("hledám index čísla -7");
+            Console.WriteLine(Linear(tab, -7));
+            #endregion
+
+            #region Binary_rek
+            Console.WriteLine("-------------Binární vyhledávání s rekurzí-------------");
+            Console.WriteLine("hledám index čísla 21");
+            Console.WriteLine(Binary_rek(tab1, 21, 0, 13));
+            Console.WriteLine("hledám index čísla 40");
+            Console.WriteLine(Binary_rek(tab1, 40, 0, 13));
+            #endregion
+
+            #region binary
+            Console.WriteLine("-------------Binární vyhledávání bez rekurze-------------");
+            Console.WriteLine("hledám index čísla 25");
+            Console.WriteLine(Binary(tab1, 25));
+            Console.WriteLine("hledám index čísla 29");
+            Console.WriteLine(Binary(tab1, 29));
+            #endregion
 
             Console.ReadKey();
         }
 
+        #region Linear
         static int Linear(int[] arr, int x)
         {
             for (int i = 0; i < arr.Length; i++)
@@ -89,7 +99,9 @@ namespace Seminárka_ALG
             }
             return -1;
         }
+        #endregion
 
+        #region Binary_rek
         static int Binary_rek(int[] arr, int x, int left, int right)
         {
             if (left > right)
@@ -110,7 +122,9 @@ namespace Seminárka_ALG
                 return Binary_rek(arr, x, mid + 1, right);
             }
         }
+        #endregion
 
+        #region Binary
         enum Nalezeno { ANO, NE };
         static int Binary(int[] arr, int x)
         {
@@ -144,7 +158,9 @@ namespace Seminárka_ALG
             else
                 return -1; // prvek nebyl nalezen, vrátí -1
         }
+        #endregion
 
+        #region Vypis
         static void Vypis(int[] arr)
         {
             foreach (int i in arr)
@@ -153,5 +169,7 @@ namespace Seminárka_ALG
             }
             Console.WriteLine();
         }
+        #endregion
     }
 }
+
